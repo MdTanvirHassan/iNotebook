237b77d71ef1d6ba4f81387332830395d0e6cff9
Tanvir Hassan <Tanvirtamim72@gmail.com>
2022-12-27 20:25:26 +0600

    iNotebook

---
+++ backend/db.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/?directConnection=true&tls=false&readPreference=primary";
+const connectToMongo =()=>{
+    mongoose.connect(mongoURI, ()=>{
+        console.log("Connected to Mongo Successfully.");
+    });
+}
+module.exports = connectToMongo;
