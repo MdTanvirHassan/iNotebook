8fb777504c98db7fa5f5b696418bd585d1159953
Tanvir Hassan <Tanvirtamim72@gmail.com>
2023-01-02 23:18:24 +0600

    Creating ThunderClient Collections to Manage Requests.

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/?directConnection=true&tls=false&readPreference=primary";
+const mongoURI = "mongodb://localhost:27017/iNotebook?directConnection=true&tls=false&readPreference=primary";
 const connectToMongo =()=>{
     mongoose.set('strictQuery', true);
     mongoose.connect(mongoURI, ()=>{
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,13 +3,13 @@ const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 
-//Create a User using : Post './api/auth/'. Doesn't require auth. 
-router.post('/',[
+//Create a User using : Post './api/auth/createUser'. Doesn't require auth. 
+router.post('/createuser',[
     body('name','Enter a valid name.').isLength({ min: 3 }),
-    body('email', 'Enter a valid email.').isEmail(),
+    body('email', 'Enter a valid email.'),
     // password must be at least 5 chars long
     body('password','password must be at least 5 chars long').isLength({ min: 5 }),
-], (req, res)=>{
+], async(req, res)=>{
     console.log(req.body);
     // const obj={
     //     a:'tanvir'
@@ -18,18 +18,32 @@ router.post('/',[
 //    user.save()
 //    res.send(req.body);
 //    //res.send(obj);
+//*If there is error, return bad request or error message
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+    //* Check Whether the email is existing already
+    try {
+    
+   let user = await User.findOne({email: req.body.email});
+   if (user) {
+    return res.status(400).json({ error: "Sorry, a user with this email already exist" })
+   }
+   //Create a new User
+   user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-    }).then(user => res.json(user))
-    .catch(err => {console.log(err)
-        res.json({error: 'Enter a valid email address', message: err.message})
     })
+    // .then(user => res.json(user))
+    // .catch(err => {console.log(err)
+    //     res.json({error: 'Enter a valid or another email address', message: err.message})
+    // })
+    res.json(user)
+} catch (error) {
+        res.status(500).send("Error Occurred. Please try again")
+    }
     })
 
 
